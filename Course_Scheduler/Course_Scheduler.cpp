ab5878fb794cd6d7cc3bccd420d2fa01b0d6a935
aderbedr <yellowsmiley7@yahoo.com>
2016-10-30 23:47:09 -0400

    Initial Commit (I think. Never used VS Git)

---
+++ Course_Scheduler/Course_Scheduler.cpp
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "scheduler.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+   if (argc < 2) {
+      std::cout << "No dependency file found.";
+      return 1;
+   }
+   TCHAR *dependencyPath = argv[1];
+
+   std::ifstream myReadFile;
+   myReadFile.open(dependencyPath);
+   if (myReadFile.is_open()) {
+      std::unordered_map<int, std::shared_ptr<Node>> nodes;
+      std::stack<std::shared_ptr<Node>> toProcess;
+      while (!myReadFile.eof()) {
+         int child = INT_MAX;
+         int parent = INT_MAX;
+         myReadFile >> child;
+         myReadFile >> parent;
+         if (child == INT_MAX || parent == INT_MAX) {
+            std::cout << "Invalid dependency file format. Format should be lines formatted of '<x> <y>', where x depends on y.";
+            return 1;
+         }
+         std::shared_ptr<Node> childNode = findOrInsert(child, nodes);
+         std::shared_ptr<Node> parentNode = findOrInsert(parent, nodes);
+         childNode->parents.push_back(parentNode);
+      }
+      myReadFile.close();
+      for (auto n : nodes){
+         toProcess.push(n.second);
+      }
+      std::deque<int> resolved;
+      while (!toProcess.empty()){
+         std::shared_ptr<Node> node = toProcess.top();
+         if (std::find(resolved.begin(), resolved.end(), node->data) == resolved.end()){
+            dep_resolve(toProcess.top(), resolved);
+         }
+         toProcess.pop();
+      }
+
+      std::deque<int>::iterator it = resolved.end();
+      while (it != resolved.begin()){
+         std::cout << *--it << std::endl;
+      }
+   }
+   return 0;
+}
+
+std::shared_ptr<Node> findOrInsert(int data, std::unordered_map<int, std::shared_ptr<Node>> &nodes){
+   std::unordered_map<int, std::shared_ptr<Node>>::const_iterator got = nodes.find(data);
+   if (got == nodes.end()){
+      std::shared_ptr<Node> parentNode = std::make_unique <Node>(data);
+      nodes.insert({ data, parentNode });
+      return parentNode;
+   }
+   else {
+      return got->second;
+   }
+}
+
+void dep_resolve(std::shared_ptr<Node> node, std::deque<int> &resolved){
+   for (std::shared_ptr<Node> neighbor : node->parents){
+      auto foo = neighbor->data;
+      auto bar = std::find(resolved.begin(), resolved.end(), foo);
+      auto end = resolved.end();
+      if (std::find(resolved.begin(), resolved.end(), neighbor->data) == resolved.end()){
+         dep_resolve(neighbor, resolved);
+      }
+   }
+   resolved.push_front(node->data);
+}
---
+++ Course_Scheduler/scheduler.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <set>
+#include <memory>
+#include <vld.h>
+
+class Node {
+public:
+   int data;
+   Node(int d) : data(d){ }
+   std::vector<std::shared_ptr<Node>> parents;
+};
+
+std::shared_ptr<Node> findOrInsert(int data, std::unordered_map<int, std::shared_ptr<Node>> &nodes);
+void dep_resolve(std::shared_ptr<Node> node, std::deque<int> &resolved);
